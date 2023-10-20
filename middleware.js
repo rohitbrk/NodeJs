2009ce4a4f243c75cd66b9bc8c2c928eb67a24c1
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2023-10-20 17:31:53 +0530

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+const PORT = 5000;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+const checkAge = (req, res, next) => {
+  if (req.query.age <= 18) res.json({ status: "error" });
+  next();
+};
+
+app.get("/api/test", checkAge, (req, res) => {
+  console.log("hey");
+  res.json({ status: "ok" });
+});
+
+// http://localhost:5000/api/test?age=12  
+// output - { status : "error" }
+
+// http://localhost:5000/api/test?age=20 
+// output - { status : "ok" }
+
+app.listen(PORT, () => {
+  console.log(`server running on ${PORT}`);
+});
