52a5829f3b77e777553d30f03f8121e7e384b8da
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2023-12-06 21:52:29 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,7 @@
+// Access Token - Short life span
+// Refresh Token - Long life span
+
+// As the user logins with credentials, an access token and a refresh token will be sent to the client.
+// For furthure requests, the client have to send those two tokens to the servers for verification.
+
+// Access token has short life span. A new access token can be issued with the help of refresh token as soon as it crosses the specified time window
