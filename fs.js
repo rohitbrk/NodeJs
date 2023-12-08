7101555586bd390a357df4923c67f64481232af0
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2023-12-08 23:07:33 +0530

    Create fs.js

---
+++ fs.js
@@ -0,0 +1,38 @@
+// fs - File System
+// Using fs built-in module, we can create folders. Read or write to files.
+
+const fs = require('fs');
+
+// read file
+  fs.readFile('test.txt', (err, data) => {
+    if (err) throw err;
+    console.log(data)
+  });
+
+// writing to a file.
+fs.writeFile('test.txt', 'Hello content!', (err) => {
+  if (err) throw err;
+  console.log('saved');
+});
+
+// appending to a file. if the file doesn't exists, the file will be created.
+fs.appendFile('test.txt', ' This is my text.', (err) => {
+  if (err) throw err;
+  console.log('updated');
+});
+
+// delete file
+fs.unlink('test.txt', (err) => {
+  if (err) throw err;
+  console.log('file deleted');
+});
+
+// remove the directory
+const dir = "/test"
+fs.rmdir(dir, err => {
+  if (err) throw err;
+  console.log(`${dir} is deleted`);
+});
+
+
+
