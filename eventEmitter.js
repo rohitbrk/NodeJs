a26104d1b134fd771c566c5181b5e8202dddc4dd
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2024-02-01 10:05:17 +0530

    Update eventEmitter.js

--- eventEmitter.js
+++ eventEmitter.js
@@ -2,12 +2,14 @@ const events = require("events");
 const eventEmitter = new events.EventEmitter();
 
 // create event handler
-const logger = () => {
-  console.log("logging info >");
+const logger = (data) => {
+	console.log("logging info > ", data.status);
 }
 
 // assign event handler
 eventEmitter.on("log", logger);
 
 // emit event
-eventEmitter.emit("log");
+eventEmitter.emit("log", {
+	status: "okay"
+});
