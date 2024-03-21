b82051650597a052e21936212c18e6f41048b00d
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2024-03-21 13:20:05 +0530

    Update basics.ts

--- typescript/basics.ts
+++ typescript/basics.ts
@@ -28,3 +28,66 @@ const empsInfo: [number, string][] = [
 	[1, 'bot1'],
 	[2, 'bot2']
 ]
+
+// union 
+let pid: number | string = 25
+pid = 'ac12'
+
+// enum
+enum DIR {
+	UP = 25,
+	DOWN = 32
+}
+// console.log(DIR.UP)
+
+// object
+type User = {
+	id: number
+	name: string
+}
+const user: User = {
+	id: 23,
+	name: 'john'
+}
+
+// type assertion 
+let cid: any = 23
+let custId2 = <number>cid
+let custId3 = cid as number
+
+// function - (return value can be void)
+function addNums(a: number, b: number): number{
+	return a+b
+}
+
+// Interface - (set optional value using "?", 
+// restrict changing valus using "readonly")
+// unions in interfaces is not possible
+interface Point {
+	readonly x: number
+	y?: number
+}
+const p1: Point = { x: 1, y: 2 }
+
+// classes
+// properties of classes can be public(accessible), 
+// private(not accessible from outside of class), 
+// protected(only within + accessible to classes that extend this class)
+// use "extends" to extend classes
+class Customer {
+	private id: number
+	name: string
+	constructor(id: number, name: string) {
+		this.id = id
+		this.name = name
+	}
+}
+
+// generics
+function makeArray<T>(items: T[]): T[] {
+	return Array().concat(items)
+}
+const numArr = makeArray<number>([1, 2, 3, 4])
+const strArr = makeArray<string>(['john', 'bill'])
+
+
