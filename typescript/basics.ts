c7f1ef41fa48cfef5907d39a59e9578926780447
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2024-03-21 11:42:54 +0530

    Update basics.ts

--- typescript/basics.ts
+++ typescript/basics.ts
@@ -6,5 +6,25 @@
 
 // Examples
 
+// number
 let n: number = 1
 console.log(n)
+
+// string
+let greeting: string = "hello world"
+
+// any
+let x: any = 1
+x = "any string"
+
+// array
+const nums: number[] = [1, 2, 3, 4, 5]
+
+// tuple
+const empInfo: [number, string] = [1, 'bot1']
+
+// tuple array
+const empsInfo: [number, string][] = [
+	[1, 'bot1'],
+	[2, 'bot2']
+]
