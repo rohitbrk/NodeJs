cc286ffbf6cba51311628c478810ffc80f4e0e15
Rohith Bathula <89332475+rohitbrk@users.noreply.github.com>
2024-03-21 11:27:01 +0530

    Create basics.ts

---
+++ typescript/basics.ts
@@ -0,0 +1,10 @@
+// Installation and Setup
+
+// npm i -D typescript (Install typescript package (specify -g to install globally))
+// tsc --init (creates tsconfig.json - edit this as per your requirements)
+// tsc (compiles the files to js)
+
+// Examples
+
+let n: number = 1
+console.log(n)
